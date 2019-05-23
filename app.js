4b5408c4b1cc02c8da67282c416963ab5fb4caf4
sangkaew bright <sangkaewbright@gmail.com>
2019-05-23 13:36:44 +0700

    file completed part7

--- app.js
+++ app.js
@@ -3,11 +3,57 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.json())
 const path = require('path')
-
-const port = 2222
-
+const port = 5555
 const db = require("./db")
 const collection = "todo"
+
+app.get('/', (req, res) => {
+    res,sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/getTodos', (req, res) => {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if (err)
+            console.log(err)
+        else {
+            console.log(documents)
+            res.json(documents)
+        }
+    })
+})
+
+app.put('/:id', (req, res) => {
+    const todoID = req.params.id
+    const userInput = req.body
+
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)}, {$set : {todo : userInput.todo}}, {returnOriginal : false}, (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            res.json(result)
+    })
+})
+
+app.post('/', (req, res) => {
+    const userInput = req.body
+    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            res.json({result : result, document : result.ops[0]})
+    })
+})
+
+app.delete('/:id', (req, res) => {
+    const todoID =req.params.id
+
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)}, (err, result) => {
+            if (err)
+                console.log(err)
+            else
+                res.json(result)
+    })
+})
 
 db.connect((err) => {
     if (err) {

53cc7f172d3043340c10c76c5f6542bfa5b664f0
sangkaew bright <sangkaewbright@gmail.com>
2019-05-23 16:33:46 +0700

    add code finish

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.json())
 const path = require('path')
+const Joi = require('joi')
 const port = 5555
 const db = require("./db")
 const collection = "todo"
+
+const schema = Joi.object().keys({
+    todo: Joi.string().required()
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -26,33 +31,53 @@ app.put('/:id', (req, res) => {
     const todoID = req.params.id
     const userInput = req.body
 
-    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)}, {$set : {todo : userInput.todo}}, {returnOriginal : false}, (err, result) => {
+    db.getDB().collection(collection).findOneAndUpdate({ _id: db.getPrimaryKey(todoID) }, { $set: { todo: userInput.todo } }, { returnOriginal: false }, (err, result) => {
         if (err)
             console.log(err)
         else
-        console.log(result)
-            res.json(result)
+            console.log(result)
+        res.json(result)
     })
 })
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
     const userInput = req.body
-    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-        if (err)
-            console.log(err)
-        else
-            res.json({result : result, document : result.ops[0]})
+
+    Joi.validate(userInput, schema, (err, result) => {
+        if (err) {
+            const error = new Error("Invalid Input")
+            error.status = 400
+            next(error)
+        } else {
+            db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+                if (err) {
+                    const error = new Error("Failed to insert Todo Document")
+                    error.status = 400
+                    next(error)
+                } else
+                    res.json({ result: result, document: result.ops[0], msg: "Successfully inserted Todo!!!", error: null })
+            })
+        }
     })
 })
+
 
 app.delete('/:id', (req, res) => {
     const todoID = req.params.id
 
-    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)}, (err, result) => {
-            if (err)
-                console.log(err)
-            else
-                res.json(result)
+    db.getDB().collection(collection).findOneAndDelete({ _id: db.getPrimaryKey(todoID) }, (err, result) => {
+        if (err)
+            console.log(err)
+        else
+            res.json(result)
+    })
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status).json({
+        error: {
+            message: err.message
+        }
     })
 })
 
@@ -62,7 +87,7 @@ db.connect((err) => {
         process.exit(1)
     } else {
         app.listen(port, () => {
-            console.log(`connected to database, app listening on port ${port}` )
+            console.log(`connected to database, app listening on port ${port}`)
         })
     }
 })
